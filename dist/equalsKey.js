a3b021add1451ff3b7918d2a6f214e5f1fe66992
srgivp <srgivp@gmail.com>
2020-04-16 12:33:09 +0300

    static info built

---
+++ dist/equalsKey.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[0],{22:function(n,e,a){"use strict";a.r(e);var c=a(1),i=a.n(c);e.default=function(n){return i.a.createElement("div",{className:"keys",id:"equals",onClick:n.onClick},"=")}}}]);
